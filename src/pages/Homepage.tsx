4d5f079d2e8858a2602391a75e2a9cd7bb784d77
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-10 15:46:46 +0600

    fix: home page

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,13 +1,13 @@
 import BannerSection from "../components/section/BannerSection";
 import FeatureSection from "../components/section/FeatureSection";
-import { Testimonials } from "../components/section/TestimonialsSection";
+// import { Testimonials } from "../components/section/TestimonialsSection";
 
 const Homepage = () => {
     return (
         <>
          <BannerSection/>  
          <FeatureSection/>
-         <Testimonials/>
+         {/* <Testimonials/> */}
         </>
     );
 };
