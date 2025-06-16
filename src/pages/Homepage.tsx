7ad159a25e6f9b6ed82e8906f43a76d6f1a4e15c
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-06-16 19:12:56 +0600

    add; home page section added

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,6 +2,8 @@ import BannerSection from "../components/section/BannerSection";
 import ZincFAQSection from "../components/section/FaqSection";
 import FeatureSection from "../components/section/FeatureSection";
 import FancyTestimonials from "../components/section/TestimonialsSection";
+import AboutUs from "./AboutUs";
+import { Events } from "./Events";
 // import { Testimonials } from "../components/section/TestimonialsSection";
 
 const Homepage = () => {
@@ -9,7 +11,9 @@ const Homepage = () => {
         <>
          <BannerSection/>  
          <FeatureSection/>
+         <Events/>
          <FancyTestimonials/>
+         <AboutUs/>
          <ZincFAQSection/>
         </>
     );
